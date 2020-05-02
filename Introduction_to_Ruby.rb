3c3d58fe9096e451de77a7a4b75c209805a83a39
paulgureghian <paulgureghian@att.net>
2020-05-02 15:56:48 -0700

    Renamed file

---
+++ Introduction_to_Ruby.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# Created by Paul A. Gureghian in May 2020. #
+# This Ruby script covers three Ruby data types: Numbers, Strings, Booleans. #
+
+# Start of script. #
+
+# Set the following variables to the proper values. #
+num = 25
+
+boolean = true
+
+string = "Ruby"
+
+# Print the variables values. #
+puts num
+puts boolean
+puts string
+
+# Math operators. #
+exponent = 2**3
+modulo = 25 % 7
+
+puts exponent
+puts modulo
+
+# 'puts and 'print'. #
+puts "Paul"
+print "Gureghian"
