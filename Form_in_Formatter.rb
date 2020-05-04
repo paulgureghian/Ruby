17d649c0efc845fdd03245d513157279749460c3
paulgureghian <paulgureghian@att.net>
2020-05-03 18:37:30 -0700

    Added string interpolation

--- Form_in_Formatter.rb
+++ Form_in_Formatter.rb
@@ -6,5 +6,21 @@
 
 # Start of script. #
 
-# Prompt the user. #
+# Prompt the user for input. #
 print "What's your first name?\n"
+first_name = gets.chomp
+
+print "What's you last name?\n"
+last_name = gets.chomp
+
+print "What's your city?\n"
+city = gets.chomp
+
+print "What's your state's abbreviation?\n"
+state = gets.chomp
+
+# Print the Output. #
+puts "Your first name is #{first_name}"
+puts "Your last name is #{last_name}"
+puts "Your city is #{city}"
+puts "Your state is #{state}"
