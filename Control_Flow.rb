9718e48c1f897c460bcf304e3f5a771648037726
paulgureghian <paulgureghian@att.net>
2020-05-05 15:47:07 -0700

    Added code comment

--- Control_Flow.rb
+++ Control_Flow.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# rubocop:disable Style/NumericPredicate
+
 # Created by Paul A. Gureghian in May 2020. #
 # This Ruby script demos control flow in Ruby. #
 
