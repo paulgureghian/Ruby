3662c494d07934e66ce8d420c2f9f6d8408df3ce
paulgureghian <paulgureghian@att.net>
2020-05-05 16:21:30 -0700

    Created script file

---
+++ This_Means_War.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Created by Paul A.Gureghian in May 2020. #
+# This Ruby script demos control flow and string methods. #
+
+# Start of script. # 
+
