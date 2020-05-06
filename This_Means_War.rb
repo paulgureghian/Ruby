40793cf505b936522a53e55fb8a433c80f1dabc6
paulgureghian <paulgureghian@att.net>
2020-05-05 18:35:16 -0700

    Fixed linter errors

--- This_Means_War.rb
+++ This_Means_War.rb
@@ -4,21 +4,21 @@
 # Created by Paul A.Gureghian in May 2020. #
 # This Ruby script demos control flow and string methods. #
 
-# Start of script. # 
+# Start of script. #
 
 # Prompt user for input. #
-print "Enter your name: "
+print 'Enter your name: '
 name = gets.chomp
 
 # Downcase the input. #
 name.downcase!
-puts 'Your name is: #{name}.'
+puts "Your name is: #{name}"
 
 # Add an 'if / else' control flow. #
-if name.include? 's' 
+if name.include? 's'
   name.gsub!(/s/, 'th')
 else
-  puts "There are no 's's in the name."  
+  puts "There are no 's's in the name." 
 end
 
 # Print the transformed input string. #
