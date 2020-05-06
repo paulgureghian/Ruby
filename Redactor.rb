e596eef051ef630c7208c6bf5c180107bd172907
paulgureghian <paulgureghian@att.net>
2020-05-06 15:26:36 -0700

    Created Ruby script file

---
+++ Redactor.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Created by Paul A.Gureghian in May 2020. #
+# This Ruby script demos the controlling of access to sensitive info in Ruby. #
+
+# Start of script. #
+
