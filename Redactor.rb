5cb2468a7ecfdd5050c9f90d1afd325b0d91b0bc
paulgureghian <paulgureghian@att.net>
2020-05-06 18:26:32 -0700

    Made minor edits

--- Redactor.rb
+++ Redactor.rb
@@ -7,14 +7,14 @@
 # Start of script. #
 
 # Prompt user for input. #
-puts "Enter text to search through: "
+puts 'Enter text to search through: '
 text = gets.chomp
 
-puts "Word to redact: "
+puts 'Word to redact: '
 redact = gets.chomp
 
 # Divide the input with the '.split' method. #
-words = text.split(" ")
+words = text.split(' ')
 
 # Use a '.each' loop to iterate through and print the words. #
 words.each { |word| puts word }
