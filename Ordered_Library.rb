779985a5b9c6da83e26bfcbba0677b697a878e55
paulgureghian <paulgureghian@att.net>
2020-05-09 18:20:24 -0700

    Created a Ruby script file

---
+++ Ordered_Library.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Created by Paul A.Gureghian in May 2020. #
+# This Ruby script demos the building of a sorting method in Ruby. #
+
+# Start of script. #
+
