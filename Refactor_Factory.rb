ff47d292bc701d3ff7c8df0bb32a1d13cae4b88f
paulgureghian <paulgureghian@att.net>
2020-05-15 15:23:37 -0700

    Created a Ruby program file

---
+++ Refactor_Factory.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Created by Paul A.Gureghian in May 2020. #
+# This Ruby program demos the refactoring of the code structure of a Ruby method. #
+
+# Start of program. #
+
