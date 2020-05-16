e118c47337176cb3321d966710e7ee6a230dbec4
paulgureghian <paulgureghian@att.net>
2020-05-16 14:48:09 -0700

    Created a Ruby program file

---
+++ Virtual_Computer.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Created by Paul A.Gureghian in May 2020. #
+# This Ruby program demos the creation of a class and the genration of instances the class. #
+
+# Start of program. #
+
