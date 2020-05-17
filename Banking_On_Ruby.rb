4f048aa1cabe9e54fd35cdff3c7c37dbe636f234
paulgureghian <paulgureghian@att.net>
2020-05-17 16:36:31 -0700

    Created a Ruby program file

---
+++ Banking_On_Ruby.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Created by Paul A.Gureghian in May 2020. #
+# This Ruby program demos the creation of a class object with public methods #
+# To display bank balances and transfer funds using a users PIN number for the transactions. #
+
+# Start of program. #
+
